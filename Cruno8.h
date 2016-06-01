5840e28db54dda5bef818f707aba16c40b8a53ee
Hemang Bhatt <hemang.bhatt@outlook.com>
2016-06-01 18:41:27 -0400

    Create Cruno8.h

---
+++ Cruno8.h
@@ -0,0 +1,43 @@
+/*********************************************************
+** Project:     CMSC 202 Project4, Spring 2016                                                                                                                                                            
+** Author:      Hemang Bhatt                                                                                                                                                                              
+** Date:        03/10/15                                                                                                                                                                                  
+** Description: This file contains Cruno8 class's functions prototypes  
+ **********************************************************/
+
+#ifndef CRUNO8_H
+#define CRUNO8_H
+
+#include "card.h"
+#include "game.h"
+#include "player.h"
+
+
+// Cruno8 class is derived class of Card Class
+
+class Cruno8: public Card {
+
+ public:
+
+  // Alternate constructor assignes suit
+  Cruno8(unsigned int suit);
+
+
+  // This virtual bool function decides whether
+  // card is playble or not
+  // This function takes Game pointer
+  virtual bool playable(Game *gptr);
+
+
+  // This virtual void function takes game pointer and player pointer
+  // and plays card
+  virtual void playCard(Game *gptr, Player *pptr);
+
+
+  // This virtual string function prints 
+  // taken action.
+  virtual string toString() ;
+};
+
+
+#endif
